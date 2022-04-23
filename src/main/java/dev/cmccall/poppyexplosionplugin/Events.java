758ce80de89c1d6cff81c6d799c9db55a2ce2b98
CarterMcCall53 <me@cmccall.dev>
2022-04-23 15:15:35 -0600

    Plugin Working Poppies have a chance to explode now

---
+++ src/main/java/dev/cmccall/poppyexplosionplugin/Events.java
@@ -0,0 +1,30 @@
+package dev.cmccall.poppyexplosionplugin;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Events implements Listener {
+    @EventHandler
+    public void onPoppyClick(PlayerInteractEvent e) {
+        if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
+            var block = e.getClickedBlock().getType();
+            if (block == Material.POPPY) {
+                var numGen = Math.random() * 100;
+                int num = (int) numGen;
+                System.out.println(num);
+                if (num == 1) {
+                    var loc = e.getClickedBlock().getLocation();
+                    var locX = e.getClickedBlock().getX();
+                    var locy = e.getClickedBlock().getY();
+                    var locZ = e.getClickedBlock().getZ();
+                    var locY = locy - 1;
+                    e.getPlayer().getWorld().createExplosion(loc, 5f);
+                }
+            }
+        }
+    }
+}
+}
---
+++ src/main/java/dev/cmccall/poppyexplosionplugin/PoppyExplosionPlugin.java
@@ -0,0 +1,18 @@
+package dev.cmccall.poppyexplosionplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PoppyExplosionPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(new Events(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
